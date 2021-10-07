85e492f3c9f7a3ccac72f35b6d0a446e0f443da9
45ewertton <ewertton45mb@gmail.com>
2021-10-07 11:57:20 -0300

    Listagem de empresas

--- company/views.py
+++ company/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Company
 # Create your views here.
 
-def home_company():
-    pass
+def home_company(request):
+
+    context = {}
+    company_all = Company.objects.all()
+    context = {'company': company_all}
+
+    return render(request, 'company.html', context)
