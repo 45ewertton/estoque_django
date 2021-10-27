b3e49755501a5ffa6e578d774ff1d94efb1241f6
45ewertton <ewertton45mb@gmail.com>
2021-10-27 15:04:29 -0300

    Sistema de alterar senha de usuario implementada, ajustes nos templates de login e register e ajustes no planejamento

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.SignUp.as_view(), name="signup"),
+    path('alter/', views.alter_user , name="alter-user"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,3 +11,19 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
+
+def alter_user(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Senha atualizada com sucesso!')
+            return redirect('alter-user')
+        else:
+            messages.error(request, 'Erro!')
+    
+    else:
+        form = PasswordChangeForm(request.user)
+    
+    return render(request, 'alter.html', {'form': form})
