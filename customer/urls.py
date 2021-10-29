bf97559fc40773341f79d3e87af8503756f33ccb
45ewertton <ewertton45mb@gmail.com>
2021-10-29 16:26:43 -0300

    Create de customer implementado sem validacoes

--- customer/urls.py
+++ customer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.CustomerView.as_view(), name='customer-list'),
+    path('create/', views.CreateCustomerView.as_view(), name='create-list'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Customer
 from .forms import CustomerForm
@@ -11,3 +12,9 @@ class CustomerView(ListView):
     template_name = 'customer.html'
     queryset = Customer.objects.all()
     context_object_name = 'customer'
+
+class CreateCustomerView(CreateView):
+    models = Customer
+    template_name = 'create.html'
+    form_class = CustomerForm
+    success_url = reverse_lazy('customer-list')
